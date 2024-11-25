3dadc0a5107eb7fd0b419d27040cf1fdb22718d3
21geeth <118731000+21geeth@users.noreply.github.com>
2024-11-25 20:34:04 +0530

    Create AllTableData.js

---
+++ AllTableData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllTableData(props) {
+    return (
+        <React.Fragment>
+            <td>{props.sname}</td>
+            <td>{props.sclass}</td>
+        </React.Fragment>
+    )
+}
+export default AllTableData
